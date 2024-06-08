02fc6d896fb5c54c7b0c487afdcaffd2d7f327af
wisnu andika <wisnuandika104@gmail.com>
2024-06-08 20:34:45 +0700

    feat: add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import './index.css'
 
 function App() {
 
   return (
     <>
-      <h1>Hallo</h1>
+     <div className='flex justify-center bg-blue-600 items-center min-h-screen'>
+      <h1 className='text-green-400 font-bold text-[52px]  text-start px-2 relative w-fit'>Hallo</h1>
+     </div>
     </>
   )
 }

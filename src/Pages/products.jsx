dd482162b3ee3bc03b39209ad9fe3f32ca2ea63c
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2024-07-03 11:44:13 +0700

    get data API

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -2,43 +2,51 @@ import React, { useEffect, useRef, useState } from 'react'
 import FirstComponentButton from '../components/Elements/Buttons/FirstComponentButton'
 import CardProduct from '../components/Fragments/CardProduct'
 import Counter from '../components/Fragments/Counter'
+import { getProducts } from '../services/product.service'
 
-const products = [
-  {
-    id: 1,
-    name: 'Sepatu Baru',
-    price: 1000000,
-    image: '/images/shoes-1.jpg',
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta iste amet illum consequuntur est suscipit quam laudantium soluta, maxime quas, id sed laborum saepe. Debitis ex reprehenderit totam fuga dolor."
-  },
-  {
-    id: 2,
-    name: 'Sepatu Lama',
-    price: 500000,
-    image: '/images/shoes-1.jpg',
-    description: "Lorem ipsum dolor sit amet consectetur"
-  },
-  {
-    id: 3,
-    name: 'Sepatu Keren',
-    price: 2000000,
-    image: '/images/shoes-1.jpg',
-    description: "Ipsum dolor sit amet consectetur"
-  },
-]
+// const products = [
+//   {
+//     id: 1,
+//     name: 'Sepatu Baru',
+//     price: 1000000,
+//     image: '/images/shoes-1.jpg',
+//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta iste amet illum consequuntur est suscipit quam laudantium soluta, maxime quas, id sed laborum saepe. Debitis ex reprehenderit totam fuga dolor."
+//   },
+//   {
+//     id: 2,
+//     name: 'Sepatu Lama',
+//     price: 500000,
+//     image: '/images/shoes-1.jpg',
+//     description: "Lorem ipsum dolor sit amet consectetur"
+//   },
+//   {
+//     id: 3,
+//     name: 'Sepatu Keren',
+//     price: 2000000,
+//     image: '/images/shoes-1.jpg',
+//     description: "Ipsum dolor sit amet consectetur"
+//   },
+// ]
 
 const email = localStorage.getItem('email')
 
 export default function ProductsPage() {
   const [cart, setCart] = useState([])
   const [totalPrice, setTotalPrice] = useState(0)
+  const [products, setProducts] = useState([])
   
   useEffect(() => {
     setCart(JSON.parse(localStorage.getItem("cart")) || [])
   }, [])
 
   useEffect(() => {
-    if(cart.length > 0) {
+    getProducts((data) => {
+      setProducts(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    if(cart.length > 0 && products.length > 0) {
       const sum = cart.reduce((acc, item) => {
         const product = products.find((product) => product.id === item.id)
         return acc + product.price * item.qty
@@ -46,7 +54,7 @@ export default function ProductsPage() {
       setTotalPrice(sum)
       localStorage.setItem("cart", JSON.stringify(cart))
     }
-  }, [cart])
+  }, [cart, products])
 
 
   const handleLogout = () => {
@@ -92,10 +100,10 @@ export default function ProductsPage() {
       </div>
       <div className='flex justify-center py-5'>
         <div className="w-4/6 flex flex-wrap">
-          {products.map((product) => (
+          {products.length > 0 && products.map((product) => (
             <CardProduct key={product.id}>
               <CardProduct.Header image={product.image}></CardProduct.Header>
-              <CardProduct.Body title={product.name}>
+              <CardProduct.Body title={product.title}>
                 {product.description}
               </CardProduct.Body>
               <CardProduct.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart} />
@@ -114,21 +122,21 @@ export default function ProductsPage() {
               </tr>
             </thead>
             <tbody>
-              {cart.map((item) => {
+              {products.length > 0 && cart.map((item) => {
                 const product = products.find((product) => product.id === item.id)
                 return(
                   <tr key={item.id}>
-                    <td>{product.name}</td>
+                    <td>{product.title.substring(0,10)} ...</td>
                     <td>Rp {product.price.toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}</td>
                     <td>{item.qty}</td>
-                    <td>Rp {(item.qty * product.price).toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}</td>
+                    <td>$ {(item.qty * product.price).toLocaleString('id-ID', {styles: 'currency', currency: 'USD'})}</td>
                   </tr>
                 )
 
               })}
               <tr  className='font-bold' ref={totalPriceRef}>
                 <td colSpan={3}>Total Price</td>
-                <td>Rp {totalPrice.toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}</td>
+                <td>$ {totalPrice.toLocaleString('id-ID', {styles: 'currency', currency: 'USD'})}</td>
               </tr>
             </tbody>
           </table>
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -12,7 +12,7 @@ export default function CardProduct({ children }) {
 const Header = ({ image }) => {
   return (
     <a href="">
-      <img src={image} alt="product" className='p-8 rounded-t-lg' />
+      <img src={image} alt="product" className='p-8 rounded-t-lg h-60 w-full object-cover' />
     </a>
   )
 }
@@ -21,9 +21,9 @@ const Body = ({children, title}) => {
   return (
     <div className='px-5 pb-5 h-full'>
       <a href="">
-        <h5 className='text-xl font-semibold tracking-high text-white'>{title}</h5>
+        <h5 className='text-xl font-semibold tracking-high text-white'>{title.substring(0,20)} ...</h5>
         <p className='text-m text-white'>
-          {children}
+          {children.substring(0,100)}
         </p>
       </a>
     </div>
@@ -34,7 +34,7 @@ const Footer = ({ price, handleAddToCart, id }) => {
   return (
     <div className='flex items-center justify-between px-5 pb-5'>
       <span className='text-xl font-bold text-white'>
-        Rp {price.toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}
+        $ {price.toLocaleString('id-ID', {styles: 'currency', currency: 'USD'})}
       </span>
       <FirstComponentButton classname='bg-blue-600' text='Add to cart' onClick={() => handleAddToCart(id)} />
     </div>
